10644727b784f7b9e483fe319db49ee9de4348c9
Pierre Nallet <pierrenallet@hotmail.com>
2015-02-24 09:52:50 -0800

    Added getclicky.

--- js/analytics.js
+++ js/analytics.js
@@ -5,3 +5,15 @@
 
   ga('create', 'UA-60024409-1', 'auto');
   ga('send', 'pageview');
+
+
+
+var clicky_site_ids = clicky_site_ids || [];
+clicky_site_ids.push(100820387);
+(function() {
+  var s = document.createElement('script');
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = '//static.getclicky.com/js';
+  ( document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0] ).appendChild( s );
+})();
